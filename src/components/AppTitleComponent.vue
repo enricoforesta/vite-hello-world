abae1cecd3deacf8e3cb6d7b85943cd450bc39d2
enricoforesta <enrico.foresta97@gmail.com>
2023-12-06 15:13:26 +0100

    feat: fine esercizio con un componente.

---
+++ src/components/AppTitleComponent.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "La mia prima App con VITE"
+    }
+  }
+}
+</script>
+
+<template>
+  <h2>{{ title }}</h2>
+</template>
+
+<style scoped>
+h2 {
+  color: yellow;
+  text-align: center;
+}
+</style>
